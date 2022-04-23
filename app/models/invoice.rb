e1d7e6a1769b912ab4f6e34007de626fe482f216
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-23 12:44:31 -0600

    Feat: merchants invoice show page has discounted revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -18,13 +18,21 @@ class Invoice < ApplicationRecord
     invoice_items.sum("unit_price * quantity")
   end
 
-  # should I be testing the best_deal method that is created in the select statement?
+  def total_discounted_revenue
+    discounted_orders = orders_that_can_be_discounted
+    if discounted_orders.empty?
+      total_revenue
+    else
+      calculate_discount_revenue(discounted_orders)
+    end
+  end
+
   def orders_that_can_be_discounted
     invoice_items.joins(item: {merchant: :bulk_discounts})
-                 .select("invoice_items.*, max(bulk_discounts.discount_percent) as best_deal")
-                 .where("invoice_items.quantity >= bulk_discounts.quantity_threshold")
-                 .group(:id)
-                 .compact
+    .select("invoice_items.*, max(bulk_discounts.discount_percent) as best_deal")
+    .where("invoice_items.quantity >= bulk_discounts.quantity_threshold")
+    .group(:id)
+    .compact
   end
 
   def self.incomplete_invoices
@@ -32,5 +40,18 @@ class Invoice < ApplicationRecord
     .where.not(invoice_items: {status: 2})
     .order(:created_at)
     .distinct
+  end
+
+private
+  def calculate_discount_revenue(discounted_orders)
+    total = 0
+    discounted_orders.each do |invoice_item|
+      total += (1.0 - invoice_item.best_deal.to_f / 100) * (invoice_item.quantity * invoice_item.unit_price)
+    end
+
+    invoice_items.each do |invoice_item|
+      total += invoice_item.quantity * invoice_item.unit_price if !discounted_orders.include?(invoice_item)
+    end
+    total.to_i
   end
 end
