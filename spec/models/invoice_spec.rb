8f7a22c741dc97fc7b8db67878552dc8516c48cd
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-23 10:29:55 -0600

    Test: invoice.orders can be discounted matches the best deal to each invoice_item

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,28 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Invoice do
-  before :each do
-    @merchant = Merchant.create!(name: 'Brylan')
-    @item_1 = @merchant.items.create!(name: 'Bottle', unit_price: 100, description: 'H20')
-    @item_2 = @merchant.items.create!(name: 'Can', unit_price: 500, description: 'Soda')
-
-    @customer = Customer.create!(first_name: "Billy", last_name: "Jonson")
-    @invoice = @customer.invoices.create!(status: "in progress")
-    @invoice_2 = @customer.invoices.create!(status: "in progress")
-    @invoice_3 = @customer.invoices.create!(status: "completed")
-
-    @invoice_item_1 = @invoice.invoice_items.create!(item_id: @item_1.id, quantity: 8, unit_price: 100, status: 'shipped')
-    @invoice_item_1a = @invoice.invoice_items.create!(item_id: @item_2.id, quantity: 5, unit_price: 500, status: 'packaged')
-    @invoice_item_2 = @invoice_2.invoice_items.create!(item_id: @item_2.id, quantity: 5, unit_price: 500, status: 'packaged')
-    @invoice_item_3 = @invoice_3.invoice_items.create!(item_id: @item_2.id, quantity: 5, unit_price: 500, status: 'shipped')
-  end
-
-  context 'readable attributes' do
-    it 'has a status' do
-      expect(@invoice.status).to eq("in progress")
-    end
-  end
-
   context 'validations' do
     it { should validate_presence_of :status}
     it { should define_enum_for(:status) }
@@ -37,12 +15,39 @@ RSpec.describe Invoice do
   end
 
   context 'instance methods' do
+    before :each do
+      @merchant = Merchant.create!(name: 'Brylan')
+      @item_1 = @merchant.items.create!(name: 'Bottle', unit_price: 100, description: 'H20')
+      @item_2 = @merchant.items.create!(name: 'Can', unit_price: 500, description: 'Soda')
+
+      @customer = Customer.create!(first_name: "Billy", last_name: "Jonson")
+      @invoice = @customer.invoices.create!(status: "in progress")
+      @invoice_2 = @customer.invoices.create!(status: "in progress")
+      @invoice_3 = @customer.invoices.create!(status: "completed")
+
+      @invoice_item_1 = @invoice.invoice_items.create!(item_id: @item_1.id, quantity: 8, unit_price: 100, status: 'shipped')
+      @invoice_item_1a = @invoice.invoice_items.create!(item_id: @item_2.id, quantity: 5, unit_price: 500, status: 'packaged')
+      @invoice_item_2 = @invoice_2.invoice_items.create!(item_id: @item_2.id, quantity: 5, unit_price: 500, status: 'packaged')
+      @invoice_item_3 = @invoice_3.invoice_items.create!(item_id: @item_2.id, quantity: 5, unit_price: 500, status: 'shipped')
+    end
+
+    it 'has a status' do
+      expect(@invoice.status).to eq("in progress")
+    end
+
     it '.get_invoice_item(id) returns a specific invoice item' do
       expect(@invoice.get_invoice_item(@item_1.id)).to eq(@invoice_item_1)
       expect(@invoice.get_invoice_item(@item_1.id)).to be_a(InvoiceItem)
 
       expect(@invoice.get_invoice_item(@item_2.id)).to eq(@invoice_item_1a)
       expect(@invoice.get_invoice_item(@item_2.id)).to be_a(InvoiceItem)
+    end
+
+    it '.incomplete_invoices can return invoices with items that have not shipped' do
+      expect(Invoice.incomplete_invoices).to eq([@invoice, @invoice_2])
+      expect(Invoice.incomplete_invoices.count).to eq(2)
+
+      expect(Invoice.incomplete_invoices).to_not include(@invoice_3)
     end
 
     it '.total_revenue returns the sum of all item costs' do
@@ -57,37 +62,51 @@ RSpec.describe Invoice do
       item_3.invoice_items.create!(invoice_id: invoice.id, quantity: 3, unit_price: 4, status: 2)
       expect(invoice.total_revenue).to eq(36)
     end
+  end
 
-    it '.discounted_revenue' do
-      merchant = Merchant.create!(name: 'Brylan')
-      merchant_2 = Merchant.create!(name: 'Chris')
+  context 'discounted revenue methods' do
+    before :each do
+      @merchant = Merchant.create!(name: 'Brylan')
+      @merchant_2 = Merchant.create!(name: 'Chris')
 
-      item_1 = merchant.items.create!(name: 'Bottle', unit_price: 100, description: 'H20')
-      item_2 = merchant.items.create!(name: 'Can', unit_price: 500, description: 'Soda')
-      item_3 = merchant_2.items.create!(name: 'Jar', unit_price: 400, description: 'Jelly')
+      @item_1 = @merchant.items.create!(name: 'Bottle', unit_price: 100, description: 'H20')
+      @item_2 = @merchant.items.create!(name: 'Can', unit_price: 500, description: 'Soda')
+      @item_3 = @merchant_2.items.create!(name: 'Jar', unit_price: 400, description: 'Jelly')
 
-      customer = Customer.create!(first_name: "Billy", last_name: "Jonson")
-      invoice_1 = customer.invoices.create!(status: "in progress", created_at: Time.parse("2022-04-12 09:54:09"))
-      invoice_2 = customer.invoices.create!(status: "in progress", created_at: Time.parse("2022-04-12 09:54:09"))
-      invoice_1.invoice_items.create!(item_id: item_1.id, status: "shipped", quantity: 8, unit_price: 100)
-      invoice_1.invoice_items.create!(item_id: item_2.id, status: "packaged", quantity: 5, unit_price: 500)
+      @customer = Customer.create!(first_name: "Billy", last_name: "Jonson")
+      @invoice_1 = @customer.invoices.create!(status: "in progress", created_at: Time.parse("2022-04-12 09:54:09"))
+      @invoice_2 = @customer.invoices.create!(status: "in progress", created_at: Time.parse("2022-04-12 09:54:09"))
+      @invoice_item_1a = @invoice_1.invoice_items.create!(item_id: @item_1.id, status: "shipped", quantity: 8, unit_price: 100)
+      @invoice_item_1b = @invoice_1.invoice_items.create!(item_id: @item_2.id, status: "packaged", quantity: 5, unit_price: 500)
+      @invoice_item_1c = @invoice_1.invoice_items.create!(item_id: @item_2.id, status: "packaged", quantity: 4, unit_price: 500)
+      @invoice_item_1d = @invoice_1.invoice_items.create!(item_id: @item_3.id, status: "packaged", quantity: 4, unit_price: 500)
 
-      merchant.bulk_discounts.create!(quantity_threshold: 3, discount_percent: 10)
-      item_1.invoice_items.create!(invoice_id: invoice_2.id, quantity: 3, unit_price: 400, status: 2)
-      item_2.invoice_items.create!(invoice_id: invoice_2.id, quantity: 3, unit_price: 400, status: 2)
-      item_3.invoice_items.create!(invoice_id: invoice_2.id, quantity: 3, unit_price: 400, status: 2)
-
-      expect(invoice_1.discounted_revenue).to eq(2970)
-      expect(invoice_1.discounted_revenue).to_not eq(3300) #total revenue of invoice_1 without discount
-      expect(invoice_1.discounted_revenue).to_not eq(3600) #total revenue of invoice_2
-      expect(invoice_1.discounted_revenue).to_not eq(6900) #total revenue of invoice_2 + invoice_1
+      @merchant.bulk_discounts.create!(quantity_threshold: 5, discount_percent: 10)
+      @merchant.bulk_discounts.create!(quantity_threshold: 2, discount_percent: 8)
+      @invoice_item_2a = @invoice_2.invoice_items.create!(item_id: @item_1.id, quantity: 3, unit_price: 400, status: 2)
+      @invoice_item_2b = @invoice_2.invoice_items.create!(item_id: @item_2.id, quantity: 3, unit_price: 400, status: 2)
+      @invoice_item_2c = @invoice_2.invoice_items.create!(item_id: @item_3.id, quantity: 3, unit_price: 400, status: 2)
     end
 
-    it '.incomplete_invoices can return invoices with items that have not shipped' do
-      expect(Invoice.incomplete_invoices).to eq([@invoice, @invoice_2])
-      expect(Invoice.incomplete_invoices.count).to eq(2)
+    it '.orders_that_can_be_discounted returns invoice_items that can qualify for a discount' do
+      expect(@invoice_1.orders_that_can_be_discounted).to eq([@invoice_item_1a, @invoice_item_1b, @invoice_item_1c])
+      expect(@invoice_1.orders_that_can_be_discounted).to_not include([@invoice_item_1d, @invoice_item_2a, @invoice_item_2b, @invoice_item_2c])
+    end
 
-      expect(Invoice.incomplete_invoices).to_not include(@invoice_3)
+    it 'invoice_items in .orders_that_can_be_discounted are matched with the highest discount_percent' do
+      qualified_orders_1 = @invoice_1.orders_that_can_be_discounted
+      invoice_item_1a = qualified_orders_1[0]
+      invoice_item_1b = qualified_orders_1[1]
+      invoice_item_1c = qualified_orders_1[2]
+
+      expect(invoice_item_1a.best_deal).to eq(10)
+      expect(invoice_item_1a.best_deal).to_not eq(8)
+
+      expect(invoice_item_1b.best_deal).to eq(10)
+      expect(invoice_item_1b.best_deal).to eq(10)
+
+      expect(invoice_item_1c.best_deal).to eq(8)
+      expect(invoice_item_1c.best_deal).to_not eq(10)
     end
   end
 end
