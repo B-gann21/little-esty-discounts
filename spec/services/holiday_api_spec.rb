107f516ccef2cf0f0bedf97c2712c1e3798143b5
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-25 14:32:15 -0600

    Test: HolidayService should convert API calls into hashes

---
+++ spec/services/holiday_api_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe HolidayService do
+  context 'instance methods' do
+    it '.get_url(url) grabs an array of hashes with symbols as keys' do
+      service = HolidayService.new
+      response = service.get_url('https://date.nager.at/api/v2/NextPublicHolidays/US')
+      keys = response.map { |holiday| holiday.keys }.flatten
+
+      expect(response).to be_a(Array)
+      expect(response[0]).to be_a(Hash)
+      expect(keys.all?(Symbol)).to be(true) 
+    end
+  end
+end
